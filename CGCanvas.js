db65c9cb9ca5f540f2c729e08b8d4a159bee5afb
corbbing <cgappdev@gmail.com>
2013-02-21 19:22:16 -0800

    Update CGCanvas.js

--- CGCanvas.js
+++ CGCanvas.js
@@ -1,1 +1,22 @@
 //CGCanvas.js
+
+//Access the canvas' "imageData.data" and change the value.
+
+
+
+function imgDataGet(x,y,ctx){
+ 
+ imgDat = ctx.getImageData(x,y,1,1);
+ return imgDat.data;
+  
+}
+
+function makeRgbaColor(){
+  
+}
+
+function imgDataGet(x,y,ctx,vArray){
+ imgDat = ctx.getImageData(x,y,1,1);
+ imgDat.data = vArray;
+ ctx.putImageData(imgDat,x,y);
+}
