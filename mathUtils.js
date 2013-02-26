03de0200bc9bccc2b68bb5ea104796084c1cfa17
corbbing <cgappdev@gmail.com>
2013-02-26 10:06:28 -0800

    Update mathUtils.js

--- mathUtils.js
+++ mathUtils.js
@@ -1,53 +1,32 @@
-//mathUtils.js
-
-/*
- * Copyright (C) 2013 Corbbin Goldsmith.
- * This code is to make math a bit quicker when dealing with complex
- * numbers
- *
- * License:
- *
- * Permission is hereby granted, free of charge, to any person 
- * obtaining a copy of this software and associated documentation files
- * (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * The Software may not be used to create training material of any sort,
- * including courses, books, instructional videos, presentations, etc.
- * without the express written consent of David Geary.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
-*/
-
-//This document will be heavily documented for full clarity
-
+//Math.js
+//
+//Corbbin Goldsmith
+//
+//National Aeronautics and Space Administration
+//
+//Copyright 2013
+//
+//Purpose: for shortening up the writing time of 
+//		   JavaScript code using complex mathem-
+//		   atics.
+//
+//Details: Building all code from the ground up.
+//		   
+//Assissted by Garett Jernigan
 
 function pt2d(x,y){
-  return{x: x, y: y}; //return the values you input, but to an array.
+	return{x: x, y: y}; //return the values you input, but to an array.
 }
 
 function pt3d(x,y,z){
 	return{x: x, y: y, z: y}; //return a 3d point as an array
 }
 
-//convert millemeters to milleseconds by the speed of sound
+//convert
 
 function mm2msec(x){
 
-	return (x) / 3433.2672;
+	return x/3433.2672;
 
 }
 
@@ -62,15 +41,13 @@ function msec2hz(x){
 //calc dist
 //sends out the distance of all items in an array
 
-// I will be using this function to calculate the distance of
-//objects and then calculating the speed of sound for the distance
+function calcDist(pt1, a, fact){
 
-function calcDist(pt1, a){
-
+	if (fact === undefined){fact=1};
 	var out= [];
 	for (var i = 0; i < a.length; ++i){
 	
-		out.push(dist(pt1, a[i]));  
+		out.push(dist(pt1, a[i]) * fact);
 		
 	}
 	return out;
@@ -84,7 +61,7 @@ function dist(pt1, pt2){
 }										 //distance of x1 and y1, and x2, and y2
 
 function rectMake(x,y,width,height){
-	return {x: x, y: y, w: width, h: height};
+	return {x: x, y: y, width: width, height: height};
 }
 
 function rect(){
@@ -98,14 +75,20 @@ function rect(){
 
 }
 
-function drawRect(rect){
+function arrayAvg(a){	
+	var d = 0; 
+	for (var i = 0; i < a.length; ++i){
 	
+		d += a[i];
+	
+	}
+	d /= a.length;
+	return d;
 }
 
 rect.draw=function(ctx){
 
 	this.bounds=rectMake(this.x, this.y, this.width, this.height);
-  //this.bounds will make it easier to access the values for collision testing, if needed.
 	ctx.save();
 	ctx.fillRect(this.bounds);
 	this.center = pt2d(getCenter(bounds)); 
@@ -115,12 +98,10 @@ rect.draw=function(ctx){
 
 function getCenter(rect){
 	return pt2d((rect.x + rect.width) /2, (rect.y + rect.height) / 2);
-  //calculates the center of a rectangle.
 }
 
 function sqr(value){
 	return value * value;
-  //x ^ 2
 }
 
 function makeColor(vec){
@@ -130,10 +111,10 @@ function makeColor(vec){
 
 function drawPoint(pt,ctx){ //x, y, context
 	ctx.save();
-	ctx.fillStyle = makeColor(pt3d(0,0,0));
+	ctx.fillStyle = "#000";
 	ctx.beginPath();
 	ctx.arc(pt.x,pt.y,3,0,Math.PI*2,true);
-	ctx.stroke
+	ctx.stroke();
 	ctx.fill();
 	ctx.restore();
 }
@@ -149,3 +130,19 @@ function dist3d(vec1, vec2){
 	
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
