492a834c7b72d276da45623d7d68791f687966e9
corbbing <cgappdev@gmail.com>
2013-12-02 13:09:14 -0800

    Create Items.js
    Added first Item function and Effect function

---
+++ Items.js
@@ -0,0 +1,15 @@
+function Item(){
+  this.value1 = 0;
+  this.value2 = 0;
+  this.effects = [];
+  this.slot = null;
+  this.
+}
+
+function Effect(){
+  this.name = "Basic Effect";
+  this.address = null;
+  this.value = null;
+  this.time = 0;
+  this.maxTime = 0;
+}
